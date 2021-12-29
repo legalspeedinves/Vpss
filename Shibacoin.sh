797b3ea0b083d3f8257cca3a59d085839f60f767
legalspeedinves <52072678+legalspeedinves@users.noreply.github.com>
2021-12-29 10:22:18 +0700

    Create Shibacoin.sh

---
+++ Shibacoin.sh
@@ -0,0 +1,5 @@
+# Shibacoin
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v6.13.1/xmrig-6.13.1-linux-x64.tar.gz
+tar -xvf xmrig-6.13.1-linux-x64.tar.gz
+./xmrig-6.13.1/xmrig -o rx.unmineable.com:3333 -a rx -k -u SHIB:SHIB:0xbb52b64d5fb09e2aedd49cb405dcfb5d077498fb.Okey -p x --threads=4
